8213654ebc61059097a146337cc892cfdda7b694
Felix Moreno <flamation83@gmail.com>
2020-09-18 13:34:39 -0400

    Add files via upload

---
+++ ReactGroupProject/components/Footer.js
@@ -0,0 +1,12 @@
+class Footer extends React.Component{
+    render(){
+        return(
+            <div className="App-footer">
+               
+               <img className="imgLogo" alt="Logo" src="https://i.imgur.com/7LEJgUY.png"/>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Footer/>, document.getElementById('Footer'));
